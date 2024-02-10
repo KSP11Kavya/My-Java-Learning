fa75aa9a6b978bb8ca3d92299b057de83a7ea949
kavya pandey <kavyapandey@kavyas-MacBook-Air.local>
2024-02-10 14:02:44 +0530

    Fourth commit

---
+++ TypeConversion/Type_conversion.java
@@ -0,0 +1,21 @@
+package TypeConversion;
+
+public class Type_conversion {
+    {
+		int i = 100;
+
+		// Automatic type conversion
+		// Integer to long type
+		long l = i;
+
+		// Automatic type conversion
+		// long to float type
+		float f = l;
+
+		// Print and display commands
+		System.out.println("Int value " + i);
+		System.out.println("Long value " + l);
+		System.out.println("Float value " + f);
+	}
+    
+}
