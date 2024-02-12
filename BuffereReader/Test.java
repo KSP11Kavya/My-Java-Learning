7391b9eb98542ced349ccc146c18712996046f51
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-12 09:52:10 +0530

    Update Test.java

--- BuffereReader/Test.java
+++ BuffereReader/Test.java
@@ -1,8 +1,6 @@
 package BuffereReader;
 import java.io.*;
-// import java.io.BufferedReader;
-// import java.io.IOException;
-// import java.io.InputStreamReader;
+
 
 public class Test {
     public static void main(String[] args) throws  IOException {
