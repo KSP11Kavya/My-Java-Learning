d608667e0d6fca58a2d0d21e53cd102983bcad76
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-23 03:52:41 +0530

    Create Main.java

---
+++ Operators/Bitwise2/Main.java
@@ -0,0 +1,10 @@
+package Operators.Bitwise2;
+
+public class Main {
+  //right shift
+    public static void main(String[] args) {
+        int x = 33;
+        System.out.println(x>>2);
+    }
+    
+}
